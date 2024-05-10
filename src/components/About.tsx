1afde0a99e0b4705566044e4a7b0ab521b7b2fe9
KOMAX233 <fajasmine@outlook.com>
2024-05-10 13:32:43 -0400

    init sections and add about and navbar

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+export default function About() {
+    return(
+        <Container id="about">
+            <div>
+                <div>
+                    <h1>Hi, I'm Yue. </h1>
+                    <p>
+                        I'm a Senior Computer Science Student at the University of Waterloo interested in ML/AI, game development and full stack engineering. 
+                        In the past, I've interned at Wireless Sensors And Devices Lab/Cloudhawk, BlackBerry, Geotab. Welcome to my website!
+                    </p>
+                    <Button></Button>
+                </div>
+            </div>
+        </Container>
+    );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import logo_white from '../assets/logo.png'
+import logo_green from '../assets/logo5.png'
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+
+export default function Navigation() {
+    return(
+        <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+        <Navbar.Brand href="#home"><Image src={logo_white} width="300" height="auto"/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#education">Education</Nav.Link>
+            {/* <NavDropdown title="Projects" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                Separated link
+                </NavDropdown.Item>
+            </NavDropdown> */}
+            <NavDropdown title="Projects" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#project/00">Shape Editing app</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Experiences" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#experience/00">Geotab</NavDropdown.Item>
+                <NavDropdown.Item href="#experience/00">Geotab</NavDropdown.Item>
+                <NavDropdown.Item href="#experience/00">Geotab</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#resume">Resume</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+        <NavbarCollapse>
+            
+        </NavbarCollapse>
+        </Container>
+    </Navbar>
+    )
+}
